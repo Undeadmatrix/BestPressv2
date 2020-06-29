a82a37f32316b5f7135b4668d6fdcf69577ea71f
Chris Little <chrisdlittle@aol.com>
2020-06-29 11:25:47 -0400

    added scratch code for home.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,12 +19,12 @@ function App() {
     <div>
       <Nav />
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/" component={Signup} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/" component={Profile} />
-        <Route exact path="/" component={Post} />
-        <Route exact path="/" component={Following} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/post" component={Post} />
+        <Route exact path="/following" component={Following} />
         <Route exact path="*" component={NoMatch} />
       </Switch>
       
---
+++ client/src/pages/home.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+
+function Home() {
+    return (
+        <Container fluid>
+            <Row>
+                <Col size="md-6">
+                    <Jumbotron>
+                        <h1>Welcome</h1>
+                    </Jumbotron>
+                </Col>
+                <Col size="md-6 sm-12">
+                    <Jumbotron>
+                        <h1>Posts</h1>
+                    </Jumbotron>
+                    {posts.length ? (
+                        <List>
+                    {posts.map(post => (
+                        <ListItem key={post._id}>
+                            <Link to={"/posts/" + post._id}>
+                                <strong>
+                                    {post.title} by {post.author}
+                                </strong>
+                            </Link>
+                            <DeleteBtn onClick={() => deletePost(post._id)} />
+                        </ListItem>
+                    ))}
+                    </List>
+                    ) : (
+                        <h3>No Results to Display</h3>
+                      )}
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Home;
