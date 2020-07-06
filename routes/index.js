210a6d882ce96d4a438bcf41f5d02c4e1f23b9d5
Chris Little <chrisdlittle@aol.com>
2020-07-06 16:26:33 -0400

    fixing heroku deploy

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,8 @@ router.get("/logout", function (req, res) {
     res.redirect("/");
   });
 // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/public/index.html"));
-// });
+ router.use(function(req, res) {
+   res.sendFile(path.join(__dirname, "../client/public/index.html"));
+ });
 
 module.exports = router;
